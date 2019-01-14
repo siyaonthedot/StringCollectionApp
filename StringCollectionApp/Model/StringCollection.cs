b4aab094c6181cb06fe93f7c1e63e2ed1ee66446
mbambosiyabonga@gmail.com <mbambosiyabonga@gmail.com>
2019-01-14 12:49:17 +0200

    initial commit

---
+++ StringCollectionApp/Model/StringCollection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StringCollectionApp.Interface;
+
+namespace StringCollectionApp.Model
+{
+    public class StringCollection : IStringCollection
+    {
+        private readonly List<string> _collection = new List<string>();
+        private string _lastFoldedString;
+        private const string delimiter = ",";
+
+        public virtual void AddString(string s)
+        {
+
+            lock (_collection)
+            {
+                _collection.Add(s);
+                _lastFoldedString = _collection.Aggregate((i, j) => i + delimiter + j);
+            }
+        }
+
+        public override string ToString()
+        {
+            return _lastFoldedString;
+        }
+    }
+}
---
+++ StringCollectionApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using StringCollectionApp.Model;
+
+
+namespace StringCollectionApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var col = new StringCollection();
+            col.AddString("White");
+            col.AddString("Red");
+            col.AddString("Black");
+            Console.WriteLine("string ouput : " + col.ToString());
+            Console.ReadKey();
+
+        }
+    }
+
+}
